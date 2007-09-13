0a0cab651daf973485ee207722a1ec1004319088
John Anderson <panic@semiosix.com>
2007-09-13 06:04:53 +0000

    allow use of db to be specified on command line

--- accounts_models.rb
+++ accounts_models.rb
@@ -3,15 +3,6 @@ require 'rubygems'
 require 'active_record'
 require 'active_record/dirty.rb'
 
-# connect to the database
-puts "using test database"
-ActiveRecord::Base.establish_connection({
-  :adapter  => 'postgresql',
-  :database => 'accounts_test',
-  :username => 'panic',
-  :password => ''
-})
-
 class Entry < ActiveRecord::Base
   include ActiveRecord::Dirty
   belongs_to :debit, :class_name => 'Account', :foreign_key => 'debit_id'
