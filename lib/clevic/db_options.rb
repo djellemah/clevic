80bbfe5d927e93e4a0bcc3039fdaa93ca3fd5b77
John Anderson <panic@semiosix.com>
2008-03-26 09:40:18 +0000

    move db options to a separate file to make irb easier

---
+++ lib/clevic/db_options.rb
@@ -0,0 +1,8 @@
+# set up defaults
+# $options[:database] to be defined with the models
+$options[:adapter]  ||= 'postgresql'
+$options[:host] ||= 'localhost'
+$options[:username] ||= 'panic'
+$options[:password] ||= ''
+
+ActiveRecord::Base.establish_connection( $options )
