e83f6aeadf8ea9c6013034ba20ef2853a92f3208
John Anderson <panic@semiosix.com>
2009-01-30 14:34:47 +0000

    more docs. Update website

--- models/minimal_models.rb
+++ models/minimal_models.rb
@@ -11,11 +11,13 @@ end
 
 # minimal definition to get combo boxes to show up
 class Entry < ActiveRecord::Base
+  include Clevic::Record
   belongs_to :debit, :class_name => 'Account', :foreign_key => 'debit_id'
   belongs_to :credit, :class_name => 'Account', :foreign_key => 'credit_id'
 end
 
 # minimal definition to get sensible values in combo boxes
 class Account < ActiveRecord::Base
+  include Clevic::Record
   def to_s; name; end
 end
