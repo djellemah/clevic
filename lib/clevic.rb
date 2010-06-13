4d52f2af377dfc7e40e8c7a21695b5c61595b0ab
John Anderson <panic@semiosix.com>
2010-06-13 17:41:53 +0000

    move meta to a plugin

--- lib/clevic.rb
+++ lib/clevic.rb
@@ -1,26 +1,8 @@
 # This provides enough to define UIs.
 
-require 'clevic/model_column.rb'
-
 module Clevic
   def self.base_entity_class
     Sequel::Model
-  end
-  
-  # define a method called meta on a Sequel::Model
-  # class so that we can get to the metadata
-  def self.define_meta( entity_class )
-    method_body = lambda do
-      if @meta.nil?
-        @meta = {}
-        db_schema.merge( association_reflections ).each do |key,value|
-          @meta[key] = ModelColumn.new( key, value )
-        end
-      end
-      @meta
-    end
-    
-    entity_class.class.send( :define_method, :meta, &method_body )
   end
 end
 
@@ -28,3 +10,4 @@ require 'clevic/sequel_ar_adapter.rb'
 require 'clevic/db_options.rb'
 require 'clevic/record.rb'
 require 'clevic/view.rb'
+require 'clevic/sequel_meta.rb'
---
+++ lib/clevic/sequel_meta.rb
@@ -0,0 +1,57 @@
+require 'sequel'
+require 'clevic/model_column.rb'
+
+module Sequel
+  module Plugins
+    module Meta
+      # plugin :meta calls this.
+      # model is the model class. The rest is whatever options are
+      # in the plugin call
+      #  plugin :meta
+      def self.configure(model, options = {})
+        model.instance_eval do
+          # store model-related stuff here
+        end
+      end
+      
+      module ClassMethods
+        # Copy the necessary class instance variables to the subclass.
+        def inherited(subclass)
+          super
+          #~ store = @cache_store
+          #~ ttl = @cache_ttl
+          #~ cache_ignore_exceptions = @cache_ignore_exceptions
+          #~ subclass.instance_eval do
+            #~ @cache_store = store
+            #~ @cache_ttl = ttl
+            #~ @cache_ignore_exceptions = cache_ignore_exceptions
+          #~ end
+        end
+        
+        def meta
+          if @meta.nil?
+            @meta = {}
+            db_schema.merge( association_reflections ).each do |key,value|
+              @meta[key] = ModelColumn.new( key, value )
+            end
+          end
+          @meta
+        end
+        
+        # reload from current metadata
+        def meta!
+          @meta = {}
+          db_schema.merge( association_reflections ).each do |key,value|
+            @meta[key] = ModelColumn.new( key, value )
+          end
+          @meta
+        end
+      end
+      
+      module InstanceMethods
+      end
+    end
+  end
+end
+
+Sequel::Model.plugin :meta
