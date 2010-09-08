77ff25dc013eb1d810d75274d7269714d1c124dc
John Anderson <panic@semiosix.com>
2010-09-08 16:20:35 +0000

    add swing-layout jar in distlist

--- distlist.sh
+++ distlist.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 export RUBYLIB=`pwd`/lib:`pwd`/models
-export CLASSPATH=$CLASSPATH:/usr/share/jdbc-postgresql/lib/jdbc-postgresql.jar
+export CLASSPATH=$CLASSPATH:/usr/share/jdbc-postgresql/lib/jdbc-postgresql.jar:`pwd`/lib/clevic/swing/ui/dist/lib/swing-layout-1.0.3.jar
 export PGHOST=groovious
 jruby bin/clevic models/times_psql_models.rb
