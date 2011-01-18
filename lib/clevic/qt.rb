a1b7fc3796be23aab8f37dfc6dea6e3273d4d8b7
John Anderson <panic@semiosix.com>
2011-01-18 14:39:30 +0200

    Start on getting qt working with Sequel, and using the API from the swing port.
    Update rakefile to create the .rb files from the Qt Designed .ui files
    
    Split delegates.rb to multiple files, one to a class
    
    binc/clevic now requires a --qt or --swing switch. Will fix this at some
    point.

---
+++ lib/clevic/qt.rb
@@ -0,0 +1,35 @@
+=begin
+  Require this file to do Clevic in Qt
+=end
+
+require 'pathname'
+
+# require these first, so TableModel and TableView get the correct ancestors
+require 'clevic/qt/table_model.rb'
+require 'clevic/qt/table_view.rb'
+( Pathname.new( __FILE__ ).parent + 'qt' ).children.grep( /.rb$/ ).each do |child|
+  require child.to_s
+end
+
+# now require the generic parts
+require 'clevic/table_model'
+require 'clevic/table_view'
+require 'clevic.rb'
+
+module Clevic
+
+def self.tahoma
+  if @font.nil?
+    @font = 
+    begin
+      found = java.awt.GraphicsEnvironment.local_graphics_environment.all_fonts.select {|f| f.font_name == "Tahoma"}.first
+      found.deriveFont( 13.0 )
+      java.awt.Font.new( 'DialogInput', java.awt.Font::PLAIN, 13 )
+  rescue
+      java.awt.Font.new( 'DialogInput', java.awt.Font::PLAIN, 13 )
+    end
+  end
+  @font
+end
+
+end
