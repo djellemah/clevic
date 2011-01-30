c944947374800f725d2000a17fc0c776ead5711f
John Anderson <panic@semiosix.com>
2011-01-30 12:19:26 +0200

    Implement with newer API

--- lib/clevic/qt/set_delegate.rb
+++ lib/clevic/qt/set_delegate.rb
@@ -4,41 +4,26 @@ module Clevic
 
 # A Combo box which allows a set of values. May or may not
 # be restricted to the set.
+# TODO this is the same as DistinctDelegate
 class SetDelegate < ComboDelegate
-  # options must contain a :set => [ ... ] to specify the set of values.
-  def initialize( field )
-    raise "RestrictedDelegate must have a :set in options" if field.set.nil?
-    super
+  def translate_from_editor_text( editor, text )
+    text
   end
   
-  def needs_combo?
-    true
-  end
-  
-  def restricted?
-    field.restricted || false
-  end
-  
-  def populate( editor, model_index )
-    field.set_for( model_index.entity ).each do |item|
-      if item.is_a?( Array )
-        # this is a hash-like set, so use key as db value
-        # and value as display value
-        editor.add_item( item.last, item.first.to_variant )
-      else
-        editor.add_item( item, item.to_variant )
-      end
+  def item_to_editor( item )
+    if item.is_a?( Array )
+      [ item.last, item.first ]
+    else
+      [ item, item ]
     end
   end
   
-  def createEditor( parent_widget, style_option_view_item, model_index )
-    editor = super
-    
-    # the set is provided, so never insert things
-    editor.insert_policy = Qt::ComboBox::NoInsert
-    editor
+  
+  def editor_to_item( data )
+    data.value
   end
-    
 end
 
 end
+
+require 'clevic/delegates/set_delegate'
