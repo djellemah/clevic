6a57615e4ed6ffe37184cab647da68fde30653c8
John Anderson <panic@semiosix.com>
2011-01-30 10:43:46 +0200

    Implement methods in Qt::ComboBox.
    Swing JComboBox will have to catch up.

--- lib/clevic/qt/combo_delegate.rb
+++ lib/clevic/qt/combo_delegate.rb
@@ -1,5 +1,54 @@
 require 'clevic/qt/delegate.rb'
 
+module Qt
+  class ComboBox
+    def no_insert=( bool )
+      self.insert_policy = Qt::ComboBox::NoInsert if bool
+    end
+    
+    def <<( item )
+      text, data = item_to_editor( item )
+      add_item( text, data.to_variant )
+    end
+    
+    def include?( item )
+      text, data = item_to_editor( item )
+      find_data( data.to_variant ) != -1
+    end
+    
+    def selected_item=( item )
+      text, data = item_to_editor( item )
+      self.current_index = find_data( data.to_variant )
+    end
+    
+    def selected_item
+      delegate.editor_to_item( item_data( self.current_index ).value )
+    end
+    
+    # wrapper for the delegate method so we don't have
+    # to keep checking for nil values
+    def item_to_editor( item )
+      if item
+        delegate.item_to_editor( item )
+      else
+        ['', nil ]
+      end
+    end
+    
+    # wrapper for the delegate method so we don't have
+    # to keep checking for nil values
+    def editor_to_item( data )
+      if data
+        delegate.editor_to_item( data )
+      else
+        nil
+      end
+    end
+    
+    attr_accessor :delegate
+  end
+end
+
 module Clevic
 
 =begin rdoc
