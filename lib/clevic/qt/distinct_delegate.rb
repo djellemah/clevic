207d05d2691bd1306479ecf8cf21dcafe97c599c
John Anderson <panic@semiosix.com>
2011-01-30 12:19:15 +0200

    Implement with newer API

--- lib/clevic/qt/distinct_delegate.rb
+++ lib/clevic/qt/distinct_delegate.rb
@@ -2,31 +2,25 @@ require 'clevic/qt/combo_delegate.rb'
 
 module Clevic
 
-# Provide a list of all values in this field,
-# and allow new values to be entered.
-# :frequency can be set as an option. Boolean. If it's true
-# the options are sorted in order of most frequently used first.
 class DistinctDelegate < ComboDelegate
-  
-  def needs_combo?
-    # works except when there is a null in the column
-    dataset.distinct.select( attribute ).count > 0
-  end
-  
-  def population
-    # we only use the first column, so use the second
-    # column to sort by, since SQL requires the order by clause
-    # to be in the select list where distinct is involved
-    #~ entity_class.adaptor.attribute_list( attribute, model_index.attribute_value, field.description, field.frequency, find_options ) do |row|
-      #~ value = row[attribute]
-      #~ editor.add_item( value, value.to_variant )
-    #~ end
-    dataset.distinct
-  end
-  
   def translate_from_editor_text( editor, text )
     text
+  end
+
+  def item_to_editor( item )
+    if item.is_a?( Array )
+      [ item.last, item.first ]
+    else
+      [ item, item ]
+    end
+  end
+  
+  
+  def editor_to_item( data )
+    data.value
   end
 end
 
 end
+
+require 'clevic/delegates/distinct_delegate.rb'
