8c608a511aadb445e2878f7548cfa41513a8c8f7
John Anderson <panic@semiosix.com>
2011-01-30 18:06:16 +0200

    First take on changes to newer API

--- lib/clevic/swing/set_delegate.rb
+++ lib/clevic/swing/set_delegate.rb
@@ -5,20 +5,18 @@ module Clevic
 # A Combo box which allows a set of values. May or may not
 # be restricted to the set.
 class SetDelegate < ComboDelegate
-  def add_to_editor( item )
-      if item.is_a?( Array )
-        puts "#{__FILE__}:#{__LINE__}:probably can't deal with item: #{item.inspect}"
-        # this is a hash-like set, so use key as db value
-        # and value as display value
-        class << item
-          def toString; last; end
-        end
-      else
-        class << item
-          def toString; self; end
-        end
+  def item_to_editor( item )
+    if item.is_a?( Array )
+      puts "#{__FILE__}:#{__LINE__}:probably can't deal with item: #{item.inspect}"
+      # this is a hash-like set, so use key as db value
+      # and value as display value
+      class << item
+        def toString; last; end
       end
-      editor << item
+    else
+      class << item
+        def toString; self; end
+      end
     end
   end
 end
