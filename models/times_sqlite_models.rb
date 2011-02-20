44d5cf6ac059a7980ab4a54646bd289747965f88
John Anderson <panic@semiosix.com>
2011-02-20 14:16:05 +0200

    Minor fixes to sqlite times_models

--- models/times_sqlite_models.rb
+++ models/times_sqlite_models.rb
@@ -14,16 +14,13 @@ Sequel.connect constring
 
 require 'times_models.rb'
 
-# Sqlite-specific code
+# Sqlite needs this plugin cos it stores Date/Times as Strings
+# and the driver doesn't translate them when loading
 
 class Entry
-  # Sqlite needs these cos it stores Date/Times as Strings
-  # and the driver doesn't translate them when loading
   plugin :typecast_on_load, :date, :start, :end
 end
 
 class Invoice
-  one_to_many :entries
-  
   plugin :typecast_on_load, :quote_date, :date
 end
