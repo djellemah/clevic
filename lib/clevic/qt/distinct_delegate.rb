026082fef8dc342770998d817e5c708f5678bfbc
John Anderson <panic@semiosix.com>
2011-02-23 15:05:17 +0200

    Fix map fields.
    translate_from_editor_text was overridden by SimplestDelegate.

--- lib/clevic/qt/distinct_delegate.rb
+++ lib/clevic/qt/distinct_delegate.rb
@@ -5,6 +5,11 @@ module Clevic
 
 class DistinctDelegate < ComboDelegate
   include SimplestDelegate
+  
+  # This might be unnecessary.
+  def translate_from_editor_text( editor, text )
+    text
+  end
 end
 
 end
