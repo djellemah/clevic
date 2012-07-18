0997b9e5fcd9324659640c8f058e289214120162
John Anderson <panic@semiosix.com>
2012-07-18 15:15:56 +0200

    make sure internal sequel metadata changes don't keep breaking clevic.

--- lib/clevic/model_column.rb
+++ lib/clevic/model_column.rb
@@ -3,13 +3,22 @@ Field metadata class. Includes information for type, reflections etc.
 
 Also, it eases the migration from AR to Sequel, which returns metadata as
 a hash instead of a class.
+
+Basically it stores a bunch of Sequel and AR specific values, and
+occasionally needs to tweak one.
 =end
 
-class ModelColumn
-  # these are from AR
-  attr_accessor :primary, :scale, :sql_type, :name, :precision, :default, :type, :meta
+require 'ostruct'
 
-  attr_writer :limit
+class ModelColumn < OpenStruct
+  def initialize( name, hash )
+    super(hash)
+
+    # must be after hash so it takes precedence
+    @name = name
+  end
+
+  attr_reader :name
 
   # if it's not here, it's probably from Sequel, so figure it out from
   # the db_type
@@ -21,57 +30,7 @@ class ModelColumn
     @limit
   end
 
-  # these are from Sequel::Model.columns_hash
-  attr_accessor :ruby_default, :primary_key, :allow_null, :db_type
-
-  # sequel::Model.reflections
-  attr_accessor :key, :eager_block, :type, :eager_grapher, :before_add, :model, :graph_join_type, :class_name, :before_remove, :eager_loader, :uses_composite_keys, :order_eager_graph, :dataset, :cartesian_product_number, :after_add, :cache, :keys, :after_remove, :extend, :graph_conditions, :name, :orig_opts, :after_load, :before_set, :after_set, :reciprocal, :reciprocal_type
-
-  # for many_to_one targets
-  attr_accessor :primary_keys
-
-  # TODO not sure where these are from
-  attr_accessor :order, :class, :conditions
-
-  # new in sequel 3.25.0
-  attr_accessor :block
-
-  # new in sequel 3.30.0
-  attr_accessor :graph_alias_base
-  attr_accessor :qualified_key
-
-  # For Sequel many_to_many
-  attr_accessor :left_key,
-    :left_keys,
-    :right_key,
-    :right_keys,
-    :left_primary_key,
-    :left_primary_keys,
-    :uses_left_composite_keys,
-    :uses_right_composite_keys,
-    :cartesian_product_number,
-    :join_table,
-    :left_key_alias,
-    :graph_join_table_conditions,
-    :graph_join_table_join_type
-
-  # added by us
-  attr_accessor :association
-  def association?; association; end
-
-  def initialize( name, hash )
-    @hash = hash
-    @hash.each do |key,value|
-      send( "#{key}=", value )
-    end
-
-    # must be after hash so it takes precedence
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  attr_writer :limit
 
   def related_class
     @related_class ||= eval class_name
