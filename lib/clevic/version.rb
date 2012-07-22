87886e24b841d9cb9dbe23fffd862975158170ac
John Anderson <panic@semiosix.com>
2012-07-22 17:31:53 +0200

    bump version

--- lib/clevic/version.rb
+++ lib/clevic/version.rb
@@ -2,7 +2,7 @@ module Clevic #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 14
-    TINY  = 2
+    TINY  = 3
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
